f6b2813beb0470f8528b880ea9e375d2391b53ff
HappinessLFZ <2659758545@qq.com>
2023-06-24 22:39:18 +0800

    信息系统开发课设——人力资源管理系统2

--- src/main/java/com/classdesign/infosystemdev/service/LoginsService.java
+++ src/main/java/com/classdesign/infosystemdev/service/LoginsService.java
@@ -1,8 +1,12 @@
 package com.classdesign.infosystemdev.service;
 
+import cn.hutool.json.JSONUtil;
 import com.classdesign.infosystemdev.dto.ResponseDTO;
 import com.classdesign.infosystemdev.entity.Staff;
 
+/**
+ * 实现github登录
+ */
 public interface LoginsService {
     ResponseDTO login(Staff staff);
 }
